a002472be603598193b64e6461d5ad253f17b09d
nianny <xiaonianhe@hotmail.com>
2021-06-19 11:20:13 +0800

    colours, colours, more colours :D

--- Quiz/ContentView.swift
+++ Quiz/ContentView.swift
@@ -38,60 +38,64 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView{
-            VStack {
-                Text(questions[currentQuestion].title)
-                    .padding()
-                
+            ZStack{
+                Image("background")
+                    .resizable()
+                    .ignoresSafeArea()
                 VStack {
-                    HStack {
-                        
-                        Button{
-                            didTapOption(optionNumber: 1)
-                        } label: {
-//                            Image(systemName: "star.fill")
-                            Text(questions[currentQuestion].option1)
+                    Text(questions[currentQuestion].title)
+                        .padding()
+                    
+                    VStack {
+                        HStack {
+                            
+                            Button{
+                                didTapOption(optionNumber: 1)
+                            } label: {
+    //                            Image(systemName: "star.fill")
+                                Text(questions[currentQuestion].option1)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
+                            .background(Color.red)
+    //                        .padding()
+                            .cornerRadius(10)
+                            Button(questions[currentQuestion].option2) {
+                                didTapOption(optionNumber: 3)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+    //                        .padding()
+                            .cornerRadius(10)
                         }
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.white)
-                        .background(Color.red)
-//                        .padding()
-                        .cornerRadius(10)
-                        Button(questions[currentQuestion].option2) {
-                            didTapOption(optionNumber: 3)
+                        HStack {
+                            Button(questions[currentQuestion].option3) {
+                                didTapOption(optionNumber: 2)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.yellow)
+                            .foregroundColor(.white)
+    //                        .padding()
+                            .cornerRadius(10)
+                            Button(questions[currentQuestion].option4) {
+                                didTapOption(optionNumber: 4)
+                            }
+                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .background(Color.green)
+                            .foregroundColor(.white)
+    //                        .padding()
+                            .cornerRadius(10)
                         }
                         .padding()
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(.white)
-                        .background(Color.blue)
-//                        .padding()
-                        .cornerRadius(10)
-                    }
-                    .padding()
-                    HStack {
-                        Button(questions[currentQuestion].option3) {
-                            didTapOption(optionNumber: 2)
-                        }
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.yellow)
-                        .foregroundColor(.white)
-//                        .padding()
-                        .cornerRadius(10)
-                        Button(questions[currentQuestion].option4) {
-                            didTapOption(optionNumber: 4)
-                        }
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.green)
-                        .foregroundColor(.white)
-//                        .padding()
-                        .cornerRadius(10)
                     }
                     .padding()
                 }
-                .padding()
-            .background(Color.black)
             }
             .navigationBarTitle("Question \(currentQuestion+1)", displayMode: .large)
         }
